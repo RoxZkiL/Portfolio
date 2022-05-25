a6eddb8c66e64a2ddeaf8e91e7084a3bfda56c0b
RoxZkiL <mttrox@gmail.com>
2022-05-25 19:59:11 -0400

    second push

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -9,9 +9,14 @@ const About = () => {
         <h3>About Me</h3>
         <p>
           I build web applications, from their data modeling to their design. I
-          am a continuous learner, curious, proactive, and tech-passionated, who
-          wants to contribute to his region's development through technology and
-          software engineering.
+          am a proactive, and tech-passionated person, who wants to contribute
+          to his region's development through technology and software
+          engineering. I am characterized by my responsibility, charisma and
+          sympathy with the people in my work group. I consider innovation and
+          helping people around me to be my passion. It should be noted that I
+          am always willing to learn new things and contribute with everything
+          within my reach so that my work environment grows and be more
+          prosperous every day.
         </p>
       </div>
       <div className="about-img">
